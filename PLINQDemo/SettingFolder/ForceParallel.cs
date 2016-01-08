c04c5627ccf6f244a1a421849d47d876b6205f28
MilesLin <jkmejkme@yahoo.com.tw>
2016-01-09 01:31:10 +0800

    WithExecutionMode

---
+++ PLINQDemo/SettingFolder/ForceParallel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PLINQDemo.SettingFolder
+{
+    public class ForceParallel
+    {
+        public void Run()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            var data = Enumerable.Range(0, 10000000)
+                .AsParallel()
+                //.WithExecutionMode(ParallelExecutionMode.ForceParallelism)
+                .Where(x => x % 2 == 0)
+                .Select((x, index) => ExpensiveMethod(x, index)).ToArray();
+            sw.Stop();
+
+            Console.WriteLine("Consume Time : {0} ms", sw.ElapsedMilliseconds);
+
+        }
+
+        private string ExpensiveMethod(int x, int index)
+        {
+            return (x * 10 + index + 2).ToString();
+        }
+
+    }
+}
